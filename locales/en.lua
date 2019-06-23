69ff61da5786398f5252bdaff9c980a4b52230ec
Jordan <51269322+jordann124@users.noreply.github.com>
2019-06-23 22:41:51 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,10 @@
+Locales['en'] = {
+	['carrega_para_apanhares_frutos'] = 'Press ~INPUT_CONTEXT~ to catch Bitcoin',
+	['carrega_para_venderes_os_sacos'] = 'Press ~INPUT_CONTEXT~ to sell the BitCoin',
+	['mochila_full'] = 'You can not get more Bicoin because your ~r~schoolbag~s~',
+	['pegar_frutos'] = '~y~A Minerar Bitcoin~s~...',
+	['nao_tens_mais_frutos'] = 'You Do not Have More ~r~Bicoin~s~',
+	['nao_tens_sacos_com_frutos'] = 'You Do not Have More ~r~Bitcoin~s~',
+	['vendeste_sacos'] = 'Sell ~y~x1~s~ ~y~Bitcoin~s~',
+	['venda_do_sacos'] = '~g~For sale Bitcoin~s~...',
+}
